11fea7def9c857f27ddb7d1d70db99bde807a784
Emin <emindedic@gmail.com>
2018-06-11 14:34:24 +0200

    Added method for domain deleting

--- zimbra_delete_accounts.rb
+++ zimbra_delete_accounts.rb
@@ -2,10 +2,10 @@
 
 require 'open3'
 
-puts "Enter domain you want to delete"
-enter_domain = gets.chomp.capitalize
-domain = "#{enter_domain}"
-
+# puts "Enter domain you want to delete"
+# enter_domain = gets.chomp.capitalize
+# domain = "#{enter_domain}"
+domain = "DOMAIN_NAME"
 
 stdout, stderr, status = Open3.capture3("zmprov -l gaa #{domain}")
 
@@ -13,4 +13,8 @@ stdout, stderr, status = Open3.capture3("zmprov -l gaa #{domain}")
 stdout.split("\n").each do |account|
   system ("zmprov da #{account}")
 end
+
+system ("zmprov dd #{domain}")
+
+print ("Accounts and domain successfully deleted")
 
